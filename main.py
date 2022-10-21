688e76abbd59ea7271291273a0d9ab6de51e3d3c
Nazar <nazarhuzner@gmail.com>
2022-10-21 22:02:30 +0300

    dz 1

---
+++ main.py
@@ -0,0 +1,8 @@
+n = input("your name(English): ")
+
+if n == 'Igor':
+    print('Hi best teacher in the world')
+#if n == 'Nazar':
+#    print('Program working(Test)')
+if n !='Igor':
+    print("Hi but this program not for you")
